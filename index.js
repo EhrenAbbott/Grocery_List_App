f40029de81bd34f9f67139b0e4c64d0d163991c2
Ehren Abbott <era12358@gmail.com>
2024-03-01 14:08:29 -0600

    Connected app to firebase database

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
     databaseURL: "https://grocerylistapp-4e6af-default-rtdb.firebaseio.com/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+
+console.log(app)
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    
+    push(moviesInDB, inputValue)
     
     console.log(`${inputValue} added to database`)
 })
